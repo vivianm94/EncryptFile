0a444fd752e397c8c1cacbecfa5ef6db51992c8c
vivianm94 <vivianmascarenhas1994@gmail.com>
2020-04-01 20:57:46 +0530

    This Application Encrypt/Decrypt Files of any types

---
+++ WpfApplication1/WpfApplication1/Class1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public sealed class Class1
+    {
+        Class1()
+        {
+
+        }
+        private static readonly object padlock = new object();
+        private static Class1 instance = null;
+        public static Class1 Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Class1();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public bool EncryptFile(string path)
+        {
+            string ext = Path.GetExtension(path).Replace(".","");
+            string filename = Path.GetFileNameWithoutExtension(path);
+
+            byte[] b= System.Text.ASCIIEncoding.ASCII.GetBytes(ext);
+            string encrypted = Convert.ToBase64String(b);
+            var newPath = path.Replace(filename+"."+ext, filename+"_"+ encrypted + ".vik");
+
+            try
+            {
+                string password = @"vik12389"; // Your Key Here
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
+
+                string cryptFile = newPath;
+                FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+   
+                RijndaelManaged RMCrypto = new RijndaelManaged();
+
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateEncryptor(key, key),
+                    CryptoStreamMode.Write);
+
+                FileStream fsIn = new FileStream(path, FileMode.Open);
+
+                int data;
+                while ((data = fsIn.ReadByte()) != -1)
+                    cs.WriteByte((byte)data);
+                fsIn.Close();
+                cs.Close();
+                fsCrypt.Close();
+                File.Delete(path);
+
+                return true;
+                
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool DecryptFile(string path)
+        {
+            string ext = Path.GetExtension(path).Replace(".", "");
+            string filename = Path.GetFileNameWithoutExtension(path);
+
+            string[] splitFilename = filename.Split('_');
+
+            byte[] b = Convert.FromBase64String(splitFilename[1]);
+
+            string decryptedfileext = System.Text.ASCIIEncoding.ASCII.GetString(b);
+
+            var newPath = path.Replace(filename + ".vik", splitFilename[0] + '.'+ decryptedfileext);
+            try
+            {
+                string password = @"vik12389"; // Your Key Here
+
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
+
+      
+                FileStream fsCrypt = new FileStream(path, FileMode.Open);
+
+                RijndaelManaged RMCrypto = new RijndaelManaged();
+
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateDecryptor(key, key),
+                    CryptoStreamMode.Read);
+
+                FileStream fsOut = new FileStream(newPath, FileMode.Create);
+
+                int data;
+                while ((data = cs.ReadByte()) != -1)
+                    fsOut.WriteByte((byte)data);
+
+                fsOut.Close();
+                cs.Close();
+                fsCrypt.Close();
+
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+        }
+
+
+
+    }
+}
---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassLibrary1;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = openFileDlg.ShowDialog();
+            
+            if (result == true)
+            {
+                richTextBox.Document.Blocks.Clear();
+                richTextBox.AppendText(openFileDlg.FileName);
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var status=Class1.Instance.EncryptFile(new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text.Trim());
+
+            if (status)
+            {
+                richTextBox.AppendText(" \n File Encrypted Sucessfully");
+            }
+            else
+            {
+                richTextBox.AppendText("\n File Encrypted Failed");
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            var status = Class1.Instance.DecryptFile(new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text.Trim());
+
+            if (status)
+            {
+                richTextBox.AppendText(" \n File Decrypted Sucessfully");
+            }
+            else
+            {
+                richTextBox.AppendText("\n File Decryption Failed");
+            }
+        }
+
+    }
+}
